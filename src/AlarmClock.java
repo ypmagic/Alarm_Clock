45eeaa1c7f760cf6543780fb972fb233df190dd7
Young Park <young_park@brown.edu>
2017-06-27 19:04:34 +0900

    Finalized output. Implemented I/O

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -1,7 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by young on 6/27/17.
  */
 public class AlarmClock {
+
+    private String result;
+
+    public AlarmClock(String input) {
+        this.result = input;
+        parseInput();
+    }
+
+    private void parseInput() {
+        String result = "It's ";
+        result += determineHour(Integer.parseInt(this.result.substring(0, 2))) + " ";
+        result += determineMinutes(Integer.parseInt(this.result.substring(3)));
+        result += determineAPm(Integer.parseInt((this.result.substring(0, 2))));
+        this.result = result;
+    }
 
     private String determineHour(int firstTwo) {
         int hour = firstTwo;
@@ -28,66 +47,87 @@ public class AlarmClock {
     private String determineMinutes(int lastTwo) {
         String[] minsToWord = new String[60];
         minsToWord[0] = "";
-        minsToWord[1] = "oh one";
-        minsToWord[2] = "oh two";
-        minsToWord[3] = "oh three";
-        minsToWord[4] = "oh four";
-        minsToWord[5] = "oh five";
-        minsToWord[6] = "oh six";
-        minsToWord[7] = "oh seven";
-        minsToWord[8] = "oh eight";
-        minsToWord[9] = "oh nine";
-        minsToWord[10] = "ten";
-        minsToWord[11] = "eleven";
-        minsToWord[12] = "twelve";
-        minsToWord[13] = "thirteen";
-        minsToWord[14] = "fourteen";
-        minsToWord[15] = "fifteen";
-        minsToWord[16] = "sixteen";
-        minsToWord[17] = "seventeen";
-        minsToWord[18] = "eighteen";
-        minsToWord[19] = "nineteen";
-        minsToWord[20] = "twenty";
-        minsToWord[21] = "twenty-one";
-        minsToWord[22] = "twenty-two";
-        minsToWord[23] = "twenty-three";
-        minsToWord[24] = "twenty-four";
-        minsToWord[25] = "twenty-five";
-        minsToWord[26] = "twenty-six";
-        minsToWord[27] = "twenty-seven";
-        minsToWord[28] = "twenty-eight";
-        minsToWord[29] = "twenty-nine";
-        minsToWord[30] = "thirty";
-        minsToWord[31] = "thirty-one";
-        minsToWord[32] = "thirty-two";
-        minsToWord[33] = "thirty-three";
-        minsToWord[34] = "thirty-four";
-        minsToWord[35] = "thirty-five";
-        minsToWord[36] = "thirty-six";
-        minsToWord[37] = "thirty-seven";
-        minsToWord[38] = "thirty-eight";
-        minsToWord[39] = "thirty-nine";
-        minsToWord[40] = "forty";
-        minsToWord[41] = "forty-one";
-        minsToWord[42] = "forty-two";
-        minsToWord[43] = "forty-three";
-        minsToWord[44] = "forty-four";
-        minsToWord[45] = "forty-five";
-        minsToWord[46] = "forty-six";
-        minsToWord[47] = "forty-seven";
-        minsToWord[48] = "forty-eight";
-        minsToWord[49] = "forty-nine";
-        minsToWord[50] = "fifty";
-        minsToWord[51] = "fifty-one";
-        minsToWord[52] = "fifty-two";
-        minsToWord[53] = "fifty-three";
-        minsToWord[54] = "fifty-four";
-        minsToWord[55] = "fifty-five";
-        minsToWord[56] = "fifty-six";
-        minsToWord[57] = "fifty-seven";
-        minsToWord[58] = "fifty-eight";
-        minsToWord[59] = "fifty-nine";
+        minsToWord[1] = "oh one ";
+        minsToWord[2] = "oh two ";
+        minsToWord[3] = "oh three ";
+        minsToWord[4] = "oh four ";
+        minsToWord[5] = "oh five ";
+        minsToWord[6] = "oh six ";
+        minsToWord[7] = "oh seven ";
+        minsToWord[8] = "oh eight ";
+        minsToWord[9] = "oh nine ";
+        minsToWord[10] = "ten ";
+        minsToWord[11] = "eleven ";
+        minsToWord[12] = "twelve ";
+        minsToWord[13] = "thirteen ";
+        minsToWord[14] = "fourteen ";
+        minsToWord[15] = "fifteen ";
+        minsToWord[16] = "sixteen ";
+        minsToWord[17] = "seventeen ";
+        minsToWord[18] = "eighteen ";
+        minsToWord[19] = "nineteen ";
+        minsToWord[20] = "twenty ";
+        minsToWord[21] = "twenty-one ";
+        minsToWord[22] = "twenty-two ";
+        minsToWord[23] = "twenty-three ";
+        minsToWord[24] = "twenty-four ";
+        minsToWord[25] = "twenty-five ";
+        minsToWord[26] = "twenty-six ";
+        minsToWord[27] = "twenty-seven ";
+        minsToWord[28] = "twenty-eight ";
+        minsToWord[29] = "twenty-nine ";
+        minsToWord[30] = "thirty ";
+        minsToWord[31] = "thirty-one ";
+        minsToWord[32] = "thirty-two ";
+        minsToWord[33] = "thirty-three ";
+        minsToWord[34] = "thirty-four ";
+        minsToWord[35] = "thirty-five ";
+        minsToWord[36] = "thirty-six ";
+        minsToWord[37] = "thirty-seven ";
+        minsToWord[38] = "thirty-eight ";
+        minsToWord[39] = "thirty-nine ";
+        minsToWord[40] = "forty ";
+        minsToWord[41] = "forty-one ";
+        minsToWord[42] = "forty-two ";
+        minsToWord[43] = "forty-three ";
+        minsToWord[44] = "forty-four ";
+        minsToWord[45] = "forty-five ";
+        minsToWord[46] = "forty-six ";
+        minsToWord[47] = "forty-seven ";
+        minsToWord[48] = "forty-eight ";
+        minsToWord[49] = "forty-nine ";
+        minsToWord[50] = "fifty ";
+        minsToWord[51] = "fifty-one ";
+        minsToWord[52] = "fifty-two ";
+        minsToWord[53] = "fifty-three ";
+        minsToWord[54] = "fifty-four ";
+        minsToWord[55] = "fifty-five ";
+        minsToWord[56] = "fifty-six ";
+        minsToWord[57] = "fifty-seven ";
+        minsToWord[58] = "fifty-eight ";
+        minsToWord[59] = "fifty-nine ";
         return minsToWord[lastTwo];
+    }
+
+    private String determineAPm(int firstTwo) {
+        if (firstTwo >= 12) {
+            return "pm";
+        } else {
+            return "am";
+        }
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        File input = new File("input.in");
+        Scanner sc = new Scanner(input);
+        while (sc.hasNext()) {
+            AlarmClock a = new AlarmClock(sc.next());
+            System.out.println(a.getResult());
+        }
     }
 
 }
